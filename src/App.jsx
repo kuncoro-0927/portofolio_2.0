e540c35eea7f4d33fe5d136f4ba897d2bf765569
kuncoro-0927 <khitanhesthik@gmail.com>
2025-09-07 20:02:50 +0700

    update app loader

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const timer = setTimeout(() => setLoading(false), 1000);
+    const timer = setTimeout(() => setLoading(false), 3000);
     return () => clearTimeout(timer);
   }, []);
 
