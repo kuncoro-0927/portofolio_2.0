447025943de6b13f3d7a4505acd33d088ff8121f
kuncoro-0927 <khitanhesthik@gmail.com>
2026-03-06 04:27:34 +0700

    update performance

--- src/data/dataproject.js
+++ src/data/dataproject.js
@@ -1,12 +1,3 @@
-import { FaCalendar } from "react-icons/fa";
-import { RiUser4Fill } from "react-icons/ri";
-import { FaLaptopCode } from "react-icons/fa";
-import { FaProjectDiagram } from "react-icons/fa";
-import { FaServer } from "react-icons/fa";
-import { FaDatabase } from "react-icons/fa";
-import { FaCreditCard } from "react-icons/fa";
-import { RiTailwindCssFill } from "react-icons/ri";
-import { MdLocalShipping } from "react-icons/md";
 const projects = [
   {
     id: 4,
@@ -16,13 +7,13 @@ const projects = [
     category: "Web Development",
     github: "https://github.com/kuncoro-0927/react-ai-chatbot",
     desc: `
-    AI Chatbot is a React-based conversational platform that integrates multiple AI models, including Google AI, OpenAI, DeepSeek AI, Anthropic AI, and xAI. The platform enables users to interact intelligently with different models in one place, providing flexibility, diverse perspectives, and a seamless AI experience.
+AI Chatbot is a React-based conversational platform that integrates multiple AI models, including Google AI, OpenAI, DeepSeek AI, Anthropic AI, and xAI. The platform enables users to interact intelligently with different models in one place, providing flexibility, diverse perspectives, and a seamless AI experience.
 `,
     slug: "react-ai-chatbot",
     type: "Capstone Project",
     mockup: "/images/react-ai/reactai1.webp",
-    // tech: "React, Google AI, Open AI, Deepseek AI, Anthropic AI, xAI",
     role: "Fullstack Developer",
+
     preview: [
       {
         title: "React AI Chatbot",
@@ -33,17 +24,21 @@ const projects = [
         images: ["/images/react-ai/ra1.webp"],
       },
     ],
+
     features: [
       "Chat with multiple AI models (Google AI, OpenAI, DeepSeek AI, Anthropic AI, and xAI) in one platform.",
       "You can choose and switch AI models based on your needs.",
       "Conversation history is saved.",
     ],
+
     details: [
-      { icon: FaCalendar, text: "2025" },
-      { icon: FaProjectDiagram, text: "Personal Project" },
+      { icon: "calendar", text: "2025" },
+      { icon: "project", text: "Personal Project" },
     ],
-    tech: [{ icon: FaLaptopCode, text: "React Js" }],
+
+    tech: [{ icon: "code", text: "React Js" }],
   },
+
   {
     id: 3,
     year: "2024",
@@ -55,20 +50,14 @@ const projects = [
     slug: "e-commerce-faza-frame",
     type: "Capstone Project",
     mockup: "/images/faza_frame/mockup.webp",
+
     preview: [
-      {
-        images: ["/images/faza_frame/ff1.webp"],
-      },
-      {
-        images: ["/images/faza_frame/ff2.webp"],
-      },
-      {
-        images: ["/images/faza_frame/ff3.webp"],
-      },
-      {
-        images: ["/images/faza_frame/ff4.webp"],
-      },
+      { images: ["/images/faza_frame/ff1.webp"] },
+      { images: ["/images/faza_frame/ff2.webp"] },
+      { images: ["/images/faza_frame/ff3.webp"] },
+      { images: ["/images/faza_frame/ff4.webp"] },
     ],
+
     features: [
       "Online Ordering: Place orders directly through the website.",
       "Product Catalog: Complete product listings with detailed information and images.",
@@ -77,20 +66,23 @@ const projects = [
       "Shipping Cost Checker: Automatic shipping cost calculation using RajaOngkir.",
       "Stock & Sales Management: Real-time inventory and sales management.",
     ],
+
     details: [
-      { icon: FaCalendar, text: "2025" },
-      { icon: RiUser4Fill, text: "Fullstack Developer" },
-      { icon: FaProjectDiagram, text: "final assignment" },
+      { icon: "calendar", text: "2025" },
+      { icon: "user", text: "Fullstack Developer" },
+      { icon: "project", text: "Final Assignment" },
     ],
+
     tech: [
-      { icon: FaLaptopCode, text: "React Js" },
-      { icon: FaServer, text: "Express Js" },
-      { icon: FaDatabase, text: "MySql" },
-      { icon: FaCreditCard, text: "Midtrans" },
-      { icon: MdLocalShipping, text: "Raja Ongkir" },
-      { icon: RiTailwindCssFill, text: "Tailwind CSS" },
+      { icon: "code", text: "React Js" },
+      { icon: "server", text: "Express Js" },
+      { icon: "database", text: "MySql" },
+      { icon: "payment", text: "Midtrans" },
+      { icon: "shipping", text: "Raja Ongkir" },
+      { icon: "tailwind", text: "Tailwind CSS" },
     ],
   },
+
   {
     id: 2,
     year: "2024",
@@ -99,105 +91,42 @@ const projects = [
     category: "Web Development",
     github: "https://github.com/kuncoro-0927",
     desc: `
-Cultivo is a web platform that helps users discover and book nature tourism and agricultural destinations across Indonesia. The website showcases various attractions, enables online ticket booking, and delivers an educational and enjoyable experience.`,
+Cultivo is a web platform that helps users discover and book nature tourism and agricultural destinations across Indonesia. The website showcases various attractions, enables online ticket booking, and delivers an educational and enjoyable experience.
+`,
     slug: "cultivo",
     type: "Internship & Independent Study Project",
     mockup: "/images/cultivo/homepage4.png",
     role: "Fullstack Developer",
+
     preview: [
-      {
-        images: ["/images/cultivo/cultivo1.webp"],
-      },
-      {
-        images: ["/images/cultivo/cultivo2.webp"],
-      },
-      {
-        images: ["/images/cultivo/cultivo3.webp"],
-      },
-      {
-        images: ["/images/cultivo/cultivo4.webp"],
-      },
+      { images: ["/images/cultivo/cultivo1.webp"] },
+      { images: ["/images/cultivo/cultivo2.webp"] },
+      { images: ["/images/cultivo/cultivo3.webp"] },
+      { images: ["/images/cultivo/cultivo4.webp"] },
     ],
+
     features: [
       "Destination Search: Easily find nature and agricultural tourism spots.",
       "Ticket Booking: Purchase tickets directly through the website.",
       "Destination Information: Detailed descriptions and images for each location.",
       "Responsive Interface: Optimized for seamless use across all devices.",
     ],
+
     details: [
-      { icon: FaCalendar, text: "2024" },
-      { icon: RiUser4Fill, text: "Fullstack Developer" },
-      { icon: FaProjectDiagram, text: "Internship" },
+      { icon: "calendar", text: "2024" },
+      { icon: "user", text: "Fullstack Developer" },
+      { icon: "project", text: "Internship" },
     ],
+
     tech: [
-      { icon: FaLaptopCode, text: "React Js" },
-      { icon: FaServer, text: "Express Js" },
-      { icon: FaDatabase, text: "MySql" },
-      { icon: FaCreditCard, text: "Midtrans" },
-      { icon: RiTailwindCssFill, text: "Tailwind CSS" },
+      { icon: "code", text: "React Js" },
+      { icon: "server", text: "Express Js" },
+      { icon: "database", text: "MySql" },
+      { icon: "payment", text: "Midtrans" },
+      { icon: "tailwind", text: "Tailwind CSS" },
     ],
   },
-  //   {
-  //     id: 3,
-  //     year: "2024",
-  //     title: "Portofolio Website",
-  //     category: "Web Development",
-  //     desc: `
-  // <em style="color:#ffffff;">Portoflio</em>
-  // <span style="color:#a6a6a6;"> is a </span>
-  // <em style="color:#ffffff;">web-based platform</em>
-  // <span style="color:#a6a6a6;"> developed for the Faculty of Vocational Studies, </span>
-  // <em style="color:#ffffff;">Universitas Brawijaya</em>.
-  // <span style="color:#a6a6a6;"> It helps secretaries efficiently </span>
-  // <em style="color:#ffffff;">record, manage, and store meeting minutes</em>
-  // <span style="color:#a6a6a6;"> in a structured and accessible way, making documentation </span>
-  // <em style="color:#ffffff;">faster, more organized,</em>
-  // <span style="color:#a6a6a6;"> and </span>
-  // <em style="color:#ffffff;">less error-prone</em>.
-  // `,
-  //     slug: "portofolio-v1",
-  //     type: "Project Based Learning",
-  //     mockup: "/images/e_notulensi/dashboardsuperadmin.png",
-  //     tech: "PHP, Laravel, Bootstrap",
-  //     role: "Backend Developer",
-  //     preview: [
-  //       {
-  //         title: "Portofolio V1",
-  //         images: ["/images/portofoliov1/mockup.webp"],
-  //       },
-  //     ],
-  //   },
 
-  //   {
-  //     id: 2,
-  //     year: "2024",
-  //     title: "iTern",
-  //     category: "Web Development",
-  //     desc: `
-  // <em style="color:#ffffff;">Portoflio</em>
-  // <span style="color:#a6a6a6;"> is a </span>
-  // <em style="color:#ffffff;">web-based platform</em>
-  // <span style="color:#a6a6a6;"> developed for the Faculty of Vocational Studies, </span>
-  // <em style="color:#ffffff;">Universitas Brawijaya</em>.
-  // <span style="color:#a6a6a6;"> It helps secretaries efficiently </span>
-  // <em style="color:#ffffff;">record, manage, and store meeting minutes</em>
-  // <span style="color:#a6a6a6;"> in a structured and accessible way, making documentation </span>
-  // <em style="color:#ffffff;">faster, more organized,</em>
-  // <span style="color:#a6a6a6;"> and </span>
-  // <em style="color:#ffffff;">less error-prone</em>.
-  // `,
-  //     slug: "itern",
-  //     type: "Project Based Learning",
-  //     mockup: "/images/e_notulensi/dashboardsuperadmin.png",
-  //     tech: "PHP, Laravel, Bootstrap",
-  //     role: "Backend Developer",
-  //     preview: [
-  //       {
-  //         title: "Dashboard",
-  //         images: ["/images/itern/itern.webp"],
-  //       },
-  //     ],
-  //   },
   {
     id: 1,
     year: "2023",
@@ -206,23 +135,18 @@ Cultivo is a web platform that helps users discover and book nature tourism and 
     category: "Web Development",
     github: "https://github.com/kuncoro-0927",
     desc: `
-E-Notulensi is a web-based platform developed for the Faculty of Vocational Studies at Universitas Brawijaya. The application assists secretaries in recording, managing, and storing meeting minutes in a structured and accessible manner, making the documentation process faster, more organized, and less prone to errors.`,
+E-Notulensi is a web-based platform developed for the Faculty of Vocational Studies at Universitas Brawijaya. The application assists secretaries in recording, managing, and storing meeting minutes in a structured and accessible manner, making the documentation process faster, more organized, and less prone to errors.
+`,
     slug: "e-notulensi",
     role: "Backend Developer",
+
     preview: [
-      {
-        images: ["/images/e_notulensi/en1.webp"],
-      },
-      {
-        images: ["/images/e_notulensi/en2.webp"],
-      },
-      {
-        images: ["/images/e_notulensi/en3.webp"],
-      },
-      {
-        images: ["/images/e_notulensi/en4.webp"],
-      },
+      { images: ["/images/e_notulensi/en1.webp"] },
+      { images: ["/images/e_notulensi/en2.webp"] },
+      { images: ["/images/e_notulensi/en3.webp"] },
+      { images: ["/images/e_notulensi/en4.webp"] },
     ],
+
     features: [
       "User Management: Manage secretary and meeting participant data (create, update, delete).",
       "Master Data Management: Manage departments and supporting system data.",
@@ -230,14 +154,16 @@ E-Notulensi is a web-based platform developed for the Faculty of Vocational Stud
       "Minutes Creation: Create and edit meeting minutes using predefined templates.",
       "Participant Management: Select meeting participants from existing records.",
     ],
+
     details: [
-      { icon: FaCalendar, text: "2023" },
-      { icon: RiUser4Fill, text: "Backend Developer" },
-      { icon: FaProjectDiagram, text: "Project Based Learning" },
+      { icon: "calendar", text: "2023" },
+      { icon: "user", text: "Backend Developer" },
+      { icon: "project", text: "Project Based Learning" },
     ],
+
     tech: [
-      { icon: FaLaptopCode, text: "Laravel" },
-      { icon: FaDatabase, text: "MySql" },
+      { icon: "code", text: "Laravel" },
+      { icon: "database", text: "MySql" },
     ],
   },
 ];
--- src/pages/DetailProject.jsx
+++ src/pages/DetailProject.jsx
@@ -1,42 +1,55 @@
-import { useNavigate } from "react-router-dom";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import projects from "../data/dataproject";
 import { FaGithub } from "react-icons/fa";
-import { BsArrowUpRight, BsArrowLeft } from "react-icons/bs";
+import { BsArrowLeft } from "react-icons/bs";
 import StarBorder from "../components/Reactbits/StarBorder";
+
+import {
+  FaCalendar,
+  FaLaptopCode,
+  FaProjectDiagram,
+  FaServer,
+  FaDatabase,
+  FaCreditCard,
+} from "react-icons/fa";
+
+import { RiUser4Fill, RiTailwindCssFill } from "react-icons/ri";
+import { MdLocalShipping } from "react-icons/md";
+
 export default function DetailProject() {
   const navigate = useNavigate();
-
   const { slug } = useParams();
   const project = projects.find((p) => p.slug === slug);
 
-  // const OPTIONS = { loop: true, align: "center", slidesToScroll: 1 };
+  const iconMap = {
+    calendar: FaCalendar,
+    user: RiUser4Fill,
+    project: FaProjectDiagram,
+    code: FaLaptopCode,
+    server: FaServer,
+    database: FaDatabase,
+    payment: FaCreditCard,
+    shipping: MdLocalShipping,
+    tailwind: RiTailwindCssFill,
+  };
 
-  // const slides = project.preview.flatMap((section, i) =>
-  //   section.images.map((img, j) => ({
-  //     id: `${i}-${j}`,
-  //     image: img,
-  //     title: `${section.title}`,
-  //   })),
-  // );
+  if (!project) return <div>Project not found</div>;
 
   return (
-    <section className="  relative z-20 mx-6 sm:mx-28 mt-10 sm:mt-10 mb-10 sm:mb-10">
+    <section className="relative z-20 mx-6 sm:mx-28 mt-10 mb-10">
       <div className="hidden md:flex">
         <StarBorder
           as="button"
           color="white"
           speed="3s"
-          className="group flex items-center justify-center gap-2 
-             rounded-full px-6 py-2 
-             text-[#a6a6a6] hover:text-white 
-             transition-colors duration-300 cursor-pointer"
+          className="group flex items-center justify-center gap-2 rounded-full px-6 py-2 text-[#a6a6a6] hover:text-white transition-colors duration-300 cursor-pointer"
           onClick={() => navigate(-1)}
         >
           <BsArrowLeft className="xl:text-xl 2xl:text-xl" />
           Back
         </StarBorder>
       </div>
+
       <div className="mt-10">
         <div className="md:flex items-center justify-between">
           <h3 className="text-2xl mb-5 md:mb-0 2xl:text-4xl font-semibold">
@@ -45,6 +58,7 @@ export default function DetailProject() {
               {project.subtitle}
             </p>
           </h3>
+
           <StarBorder
             as="a"
             color="white"
@@ -52,17 +66,16 @@ export default function DetailProject() {
             href={project.github}
             target="_blank"
             rel="noopener noreferrer"
-            className="group flex items-center justify-center gap-2 
-             rounded-full px-6 py-2 
-             text-[#a6a6a6] hover:text-white 
-             transition-colors duration-300 cursor-pointer"
+            className="group flex items-center justify-center gap-2 rounded-full px-6 py-2 text-[#a6a6a6] hover:text-white transition-colors duration-300 cursor-pointer"
           >
             <FaGithub className="xl:text-xl 2xl:text-xl" />
             Github
           </StarBorder>
         </div>
 
-        <div className="mt-5 2xl:mt-10 items-stretch grid grid-cols-1 md:grid-cols-2 gap-4">
+        {/* PREVIEW IMAGES */}
+
+        <div className="mt-5 2xl:mt-10 grid grid-cols-1 md:grid-cols-2 gap-4">
           {project.preview.map((section, i) =>
             section.images.map((img, j) => (
               <img
@@ -77,61 +90,60 @@ export default function DetailProject() {
           )}
         </div>
 
-        {/* 
-          <div className="flex mt-5 sm:mt-5 gap-3 flex-wrap">
-            {project.tech.split(",").map((item, index) => (
-              <span
-                key={index}
-                className="border text-sm px-3 py-2 2xl:px-4 2xl:py-2.5 rounded-3xl"
-              >
-                {item.trim()}
-              </span>
-            ))}
-          </div> */}
-        <div className="mt-10 md:flex items-start justify-start gap-20 sm:mt-10">
+        <div className="mt-10 md:flex items-start gap-20">
+          {/* ABOUT */}
+
           <div className="md:max-w-3xl 2xl:max-w-4xl">
             <h2 className="text-2xl 2xl:text-3xl font-semibold">
               About {project.title}
             </h2>
+
             <p
-              className="text-[#a6a6a6] text-md mt-4 2xl:mt-4 2xl:text-base"
+              className="text-[#a6a6a6] text-md mt-4 2xl:text-base"
               dangerouslySetInnerHTML={{ __html: project.desc }}
             />
 
-            <p className="text-[#a6a6a6] mt-4 2xl:mt-4">Main Features:</p>
-            <ul className="list-disc space-y-0.5 ml-4 2xl:mt-4 text-[#a6a6a6]">
+            <p className="text-[#a6a6a6] mt-4">Main Features:</p>
+
+            <ul className="list-disc space-y-1 ml-4 text-[#a6a6a6]">
               {project.features.map((feature, index) => (
                 <li key={index}>{feature}</li>
               ))}
             </ul>
           </div>
 
-          <div className="text-base mt-4 md:mt-0">
+          {/* DETAILS */}
+
+          <div className="text-base mt-6 md:mt-0">
             <h2 className="mb-2">Details</h2>
+
             {project.details.map((detail, i) => {
-              const Icon = detail.icon;
+              const Icon = iconMap[detail.icon];
+
               return (
                 <div
                   key={i}
-                  className="flex items-center gap-4 text-[#a6a6a6] my-0.5 text-base"
+                  className="flex items-center gap-4 text-[#a6a6a6] my-1"
                 >
-                  <Icon className="text-[#a6a6a6]" />
-
+                  {Icon && <Icon />}
                   <span>{detail.text}</span>
                 </div>
               );
             })}
 
+            {/* TECH STACK */}
+
             <h2 className="mb-2 mt-6">Tech Stack</h2>
+
             {project.tech.map((detail, i) => {
-              const Icon = detail.icon;
+              const Icon = iconMap[detail.icon];
+
               return (
                 <div
                   key={i}
-                  className="flex items-center gap-4 text-[#a6a6a6] my-0.5 text-base"
+                  className="flex items-center gap-4 text-[#a6a6a6] my-1"
                 >
-                  <Icon className="text-[#a6a6a6]" />
-
+                  {Icon && <Icon />}
                   <span>{detail.text}</span>
                 </div>
               );
